b088fb6acb2bf51deca9d2c544e1ab3df06d220d
user <user@mail.com>
2017-07-19 15:10:44 -0500

    no es una propiedad

--- test.py
+++ test.py
@@ -5,8 +5,15 @@ from Monopolio import *
 #from Juego import Juego
 
 class Test(unittest.TestCase):
-    #Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    #Test1 valida cuando no es una propiedad
     def test1(self):
+        celda1 = Celda("", 1, "Banco", 150, 50)
+        persona1 = Persona("Toreto", 160,"si")
+        juego1 = Juego()
+        msg = juego1.validar_movimiento(celda1,persona1)
+        self.assertEquals(msg,"No Valido")
+    #Test3 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    def test3(self):
         celda3 = Celda("", 1, "Propiedad", 150, 50)
         persona3 = Persona("Toreto", 160,"si")
         juego3 = Juego()
