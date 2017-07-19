b5b18fdcddecbfbc14afe4512bdc9efc4e016918
Erick Pérez <erialper@espol.edu.ec>
2017-07-19 15:38:15 -0500

    pruebas

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ class Test(unittest.TestCase):
         persona3 = Persona("Toreto", 160,"si")
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
-        self.assertEquals(msg,"la celda no es una Propiedad")
+        self.assertEquals(msg,"No Valido")
     # Test3 valida cuando es una propiedad, no tiene dinero.
     def test3(self):
         celda3 = Celda("", 1, "Propiedad", 150, 50)
