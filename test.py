55783cc69a4ef92fa1eabae74fd0d23d76b06fef
ivanmosquera <ivan11mosquera@gmail.com>
2017-07-19 15:35:24 -0500

    Añadiendo segunda prueba

--- test.py
+++ test.py
@@ -13,11 +13,20 @@ class Test(unittest.TestCase):
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
 
+    # Test2 valida cuando no es una propiedad, tiene dinero y quiere comprarla.
     def test2(self):
         celda4 = Celda("", 2, "No Propiedad", 0, 0)
         persona4 = Persona("Toreto", 160,"si")
         juego4 = Juego()
         msg = juego4.validar_movimiento(celda4,persona4)
         self.assertEquals(msg,"la celda no es una Propiedad")
+
+    # Test3 valida cuando es una propiedad, no tiene dueño, no tiene dinero suficiente para comprar entonces sale del juego.
+    def test3(self):
+        celda3 = Celda("", 1, "Propiedad", 200, 50)
+        persona3 = Persona("Toreto", 160,"si")
+        juego3 = Juego()
+        msg = juego3.validar_movimiento(celda3,persona3)
+        self.assertEquals(msg,"No tiene dinero suficiente")
 if __name__ == '__main__':
     unittest.main()
