1677bb2d39b0393e453e5ae63df47cf72c5c71bd
Juan Alvarado <CltControl@GGIECSOFWRK147.espol.edu.ec>
2017-07-19 15:42:57 -0500

    Indentation

--- Monopolio.py
+++ Monopolio.py
@@ -7,15 +7,6 @@ class Persona:
         self.monto = monto
         self.comprar = comprar
 
-class Persona:
-  nombre = ""
-  monto = 0
-  comprar = ""
-  def __init__(self, nombre, monto,comprar):
-        self.nombre = nombre
-        self.monto = monto
-        self.comprar = comprar
-
 class Celda:
   dueno = ""
   numero= 0
@@ -31,35 +22,33 @@ class Celda:
 
 
 class Juego:
-    def validar_movimiento(self,celda,turnoPersona):
-      if (celda.tipo!="Propiedad"):#1
-         # se ejecuta otro codigo
-         print("la celda no es una Propiedad")#2
-       return "No Valido"
+  def validar_movimiento(self,celda,turnoPersona):
+    if (celda.tipo!="Propiedad"):#1
+      # se ejecuta otro codigo
+      print("la celda no es una Propiedad")#2
+      return "No Valido"
+    else:
+      if celda.dueno is "":#3
+        if turnoPersona.monto < celda.precio:#4
+          print("salio del juego")#5
+          return "No tiene dinero suficiente"
+        else:
+          if turnoPersona.comprar=="si":#6
+            #puede comprar
+            print("paga para ser el dueno")#7
+            turnoPersona.monto = turnoPersona.monto - celda.precio
+            return "Usted ha comprado una propiedad"
+          else:
+            return "Sigue jugando"
       else:
-        if celda.dueno is "":#3
-          if turnoPersona.monto < celda.precio:#4
-              print("salio del juego")#5
-              return "No tiene dinero suficiente"
-          else:
-        if turnoPersona.comprar=="si":#6
-          #puede comprar
-          print("paga para ser el dueno")#7
-          turnoPersona.monto = turnoPersona.monto - celda.precio
-          return "Usted ha comprado una propiedad"
+        if celda.dueno == turnoPersona.nombre:#8
+          print("es el mismo dueno")#9
+          return "Esta proiedad es suya, continua jugando"
         else:
-          return "Sigue jugando"
-        else:
-          if celda.dueno == turnoPersona.nombre:#8
-              print("es el mismo dueno")#9
-              return "Esta proiedad es suya, continua jugando"
+          if turnoPersona.monto < celda.renta:#10
+            print("pierde el juego no tiene dinero para pagar")#11
+            return "Perdio"
           else:
-            if turnoPersona.monto < celda.renta:#10
-                print("pierde el juego no tiene dinero para pagar")#11
-                return "Perdio"
-            else:
-                print("paga la renta")#12
-                turnoPersona.monto = turnoPersona.monto - celda.renta
-                return "Usted pago renta"
-
-    
+            print("paga la renta")#12
+            turnoPersona.monto = turnoPersona.monto - celda.renta
+            return "Usted pago renta"
