950c60babeb514d60596bbee6c253cda5210fefc
user <user@mail.com>
2017-07-19 15:35:25 -0500

    return completo

--- test.py
+++ test.py
@@ -26,6 +26,33 @@ class Test(unittest.TestCase):
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
-    #Test4
+    #Test4 valida cuando es una propiedad, tiene dinero, decide no comprar la propiedad entonces continua en el juego.
+    def test4(self):
+        celda4 = Celda("", 1, "Propiedad", 150, 50)
+        persona4 = Persona("Toreto", 160,"no")
+        juego4 = Juego()
+        msg = juego4.validar_movimiento(celda4,persona4)
+        self.assertEquals(msg,"Sigue jugando")
+    #Test5 valida cuando es una propiedad, tiene dueno y es el participante
+    def test5(self):
+        celda5 = Celda("Toreto", 1, "Propiedad", 150, 50)
+        persona5 = Persona("Toreto", 160,"si")
+        juego5 = Juego()
+        msg = juego5.validar_movimiento(celda5,persona5)
+        self.assertEquals(msg,"Esta proiedad es suya, continua jugando")
+    #Test6 valida cuando es una propiedad, tiene dueno y no tiene dinero para pagar la renta
+    def test6(self):
+        celda6 = Celda("Jose", 1, "Propiedad", 150, 50)
+        persona6 = Persona("Toreto", 40,"si")
+        juego6 = Juego()
+        msg = juego6.validar_movimiento(celda6,persona6)
+        self.assertEquals(msg,"Perdio")
+    #Test7 valida cuando es una propiedad, tiene dueno y si tiene dinero para pagar la renta
+    def test(self):
+        celda7 = Celda("Jose", 1, "Propiedad", 150, 50)
+        persona7 = Persona("Toreto", 60,"si")
+        juego7 = Juego()
+        msg = juego7.validar_movimiento(celda7,persona7)
+        self.assertEquals(msg,"Usted pago renta")
 if __name__ == '__main__':
     unittest.main()
