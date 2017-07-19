0d161bd5ce579bcb5ef8f547dea7ba1e63b30322
Juan Alvarado <CltControl@GGIECSOFWRK147.espol.edu.ec>
2017-07-19 15:31:57 -0500

    WhiteBox Testing

--- Monopolio.py
+++ Monopolio.py
@@ -22,33 +22,33 @@ class Celda:
 
 
 class Juego:
-    def validar_movimiento(self,celda,turnoPersona):
-      if (celda.tipo!="Propiedad"):
-         # se ejecuta otro codigo
-         print("la celda no es una Propiedad")
-    	 return "No Valido"
-      else:
-        if celda.dueno is "":
-          if turnoPersona.monto < celda.precio:
-              print("salio del juego")
-              return "No tiene dinero suficiente"
-          else:
-    		if turnoPersona.comprar=="si":
-    			#puede comprar
-    			print("paga para ser el dueno")
-    			turnoPersona.monto = turnoPersona.monto - celda.precio
-    			return "Usted ha comprado una propiedad"
-    		else:
-    			return "Sigue jugando"
+  def validar_movimiento(self,celda,turnoPersona):
+    if (celda.tipo!="Propiedad"):#1
+      # se ejecuta otro codigo
+      print("la celda no es una Propiedad")#2
+      return "No Valido"#End
+    else:
+      if celda.dueno is "":#3
+        if turnoPersona.monto < celda.precio:#4
+          print("salio del juego")#5
+          return "No tiene dinero suficiente"#End
         else:
-          if celda.dueno == turnoPersona.nombre:
-              print("es el mismo dueno")
-              return "Esta proiedad es suya, continua jugando"
+          if turnoPersona.comprar=="si":#6
+            #puede comprar
+            print("paga para ser el dueno")#7
+            turnoPersona.monto = turnoPersona.monto - celda.precio
+            return "Usted ha comprado una propiedad"#End
           else:
-            if turnoPersona.monto < celda.renta:
-                print("pierde el juego no tiene dinero para pagar")
-                return "Perdio"
+          return "Sigue jugando"#End
+        else:
+          if celda.dueno == turnoPersona.nombre:#8
+              print("es el mismo dueno")#9
+              return "Esta proiedad es suya, continua jugando"#End
+          else:
+            if turnoPersona.monto < celda.renta:#10
+                print("pierde el juego no tiene dinero para pagar")#11
+                return "Perdio"#End
             else:
-                print("paga la renta")
+                print("paga la renta")#12
                 turnoPersona.monto = turnoPersona.monto - celda.renta
-                return "Usted pago renta"
+                return "Usted pago renta"#End
--- test.py
+++ test.py
@@ -5,12 +5,48 @@ from Monopolio import *
 #from Juego import Juego
 
 class Test(unittest.TestCase):
-    #Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
-    def test1(self):
-        celda3 = Celda("", 1, "Propiedad", 150, 50)
-        persona3 = Persona("Toreto", 160,"si")
-        juego3 = Juego()
-        msg = juego3.validar_movimiento(celda3,persona3)
-        self.assertEquals(msg,"Usted ha comprado una propiedad")
+	#Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+	def test1(self):
+		celda1 = Celda("", 1, "Propiedad", 150, 50)
+		persona1 = Persona("Toreto", 160,"si")
+		juego1 = Juego()
+		msg = juego3.validar_movimiento(celda1,persona1)
+		self.assertEquals(msg,"Usted ha comprado una propiedad")
+	def test2(self):
+		celda2 = Celda("",1,"GO", 150, 50)
+		persona2 = Persona("Toreto", 160,"si")
+		juego2 = Juego()
+		msg = juego2.validar_movimiento(celda2,persona2)
+		self.assertEquals(msg,"la celda no es una Propiedad")
+	def test3(self):
+		celda3 = Celda("",1,"Propiedad", 150, 50)
+		persona3 = Persona("Toreto", 10,"si")
+		juego3 = Juego()
+		msg = juego3.validar_movimiento(celda3,persona3)
+		self.assertEquals(msg,"No tiene dinero suficiente")
+	def test4(self):
+		celda4 = Celda("",1,"Propiedad", 150, 50)
+		persona4 = Persona("Toreto", 160,"no")
+		juego4 = Juego()
+		msg = juego4.validar_movimiento(celda4,persona4)
+		self.assertEquals(msg,"Sigue jugando")
+	def test5(self):
+		celda5 = Celda("Toreto",1,"Propiedad", 150, 50)
+		persona5 = Persona("Toreto", 160,"si")
+		juego5 = Juego()
+		msg = juego5.validar_movimiento(celda5,persona5)
+		self.assertEquals(msg,"Esta proiedad es suya, continua jugando")
+	def test6(self):
+		celda6 = Celda("Letty",1,"Propiedad", 150, 50)
+		persona6 = Persona("Toreto", 10,"si")
+		juego6 = Juego()
+		msg = juego6.validar_movimiento(celda6,persona6)
+		self.assertEquals(msg,"pierde el juego no tiene dinero para pagar")
+	def test7(self):
+		celda7 = Celda("Letty",1,"Propiedad", 150, 50)
+		persona7 = Persona("Toreto", 170,"si")
+		juego7 = Juego()
+		msg = juego7.validar_movimiento(celda7,persona7)
+		self.assertEquals(msg,"Usted pago renta")
 if __name__ == '__main__':
     unittest.main()
