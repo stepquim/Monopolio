6aed68e7545448744daf7ad9ec72942cacb99fe7
LuisBSC15 <CltControl@GGIECSOFWRK159.espol.edu.ec>
2017-07-19 15:56:13 -0500

    TestYagual

--- test.py
+++ test.py
@@ -23,11 +23,11 @@ class Test(unittest.TestCase):
 
     #Test3 valida cuando  es una propiedad y no tiene suficiente dinero
     def test3(self):
-        celda3 = Celda("Alan", 1, "Propiedad", 250, 130)
+        celda3 = Celda("", 1, "Propiedad", 250, 30)
         persona3 = Persona("Edison", 100,"si")
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
-        self.assertEqual(msg,"Perdio")
+        self.assertEqual(msg,"No tiene dinero suficiente")
 
 
     #Test4 valida cuando  es una propiedad y ya es mi propiedad
@@ -39,23 +39,22 @@ class Test(unittest.TestCase):
         self.assertEqual(msg,"Esta proiedad es suya, continua jugando")
 
 
-    #Test5 valida cuando  es una propiedad,tiene dueño y no tengo dinero para la renta
+    #Test5 valida cuando  es una propiedad, y no tengo dinero para la renta
     def test5(self):
-        celda3 = Celda("Toreto", 1, "Propiedad", 250, 130)
+        celda3 = Celda("", 1, "Propiedad", 250, 130)
         persona3 = Persona("Sara", 100,"si")
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
-        self.assertEqual(msg,"Perdio")
+        self.assertEqual(msg,"No tiene dinero suficiente")
 
 
-
-    #Test6 valida cuando  es una propiedad,tiene dueño y aun tengo dinero para la renta
+    #Test6 valida cuando  es una propiedad,y aun tengo dinero para la renta
     def test6(self):
-        celda3 = Celda("Toreto", 1, "Propiedad", 250, 130)
-        persona3 = Persona("Alan", 200,"si")
+        celda3 = Celda("", 1, "Propiedad", 200, 50)
+        persona3 = Persona("Alan", 200,"no")
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
-        self.assertEqual(msg,"Usted pago renta")
+        self.assertEqual(msg,"Sigue jugando")
 
     #Test7 valida cuando  es una propiedad, no tiene dueño y  tengo dinero pero no se desea comprar
     def test7(self):
