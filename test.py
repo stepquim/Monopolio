87e8e6fcda83f278921f8abf1f778f23e41ce7f8
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-07-19 14:48:17 -0500

    Prueba
    1

--- test.py
+++ test.py
@@ -12,5 +12,9 @@ class Test(unittest.TestCase):
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
+
+    #Test2 valida cuando es una propiedad y no tiene dindero
+    def test2(self):
+        pass
 if __name__ == '__main__':
     unittest.main()
