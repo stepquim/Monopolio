dba8d183c2c03c18c91b869f04d73a9c803abd41
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-07-19 15:18:44 -0500

    Agregado 3 tests mas

--- test.py
+++ test.py
@@ -37,5 +37,32 @@ class Test(unittest.TestCase):
         msg = juego3.validar_movimiento(celda3, persona3)
         self.assertEquals(msg, "No Valido")
 
+
+     # Test 5 valida cuando la propiedad en la que cae ya tiene duenho, pero es el mismo jugador
+    def test5(self):
+        celda3 = Celda("Carlos", 1, "Propiedad", 150, 50)
+        persona3 = Persona("Carlos", 160, "No")
+        juego=Juego()
+        msg=juego.validar_movimiento(celda3,persona3)
+        self.assertEquals(msg,"Esta proiedad es suya, continua jugando")
+
+
+    # Test 6 valida cuando la propiedad en la que cae ya tiene duenho, pero el jugador no puede pagar el valor de la renta
+
+    def test6(self):
+        celda3 = Celda("Carlos", 1, "Propiedad", 500, 250)
+        persona3 = Persona("Luis", 160, "No")
+        juego = Juego()
+        msg = juego.validar_movimiento(celda3, persona3)
+        self.assertEquals(msg, "Perdio")
+
+    # Test 7 valida cuando la propiedad en la que cae ya tiene duenho, y el jugador paga la renta
+    def test7(self):
+        celda3 = Celda("Carlos", 1, "Propiedad", 400, 150)
+        persona3 = Persona("Luis", 160, "No")
+        juego = Juego()
+        msg = juego.validar_movimiento(celda3, persona3)
+        self.assertEquals(msg, "Usted pago renta")
+
 if __name__ == '__main__':
     unittest.main()
