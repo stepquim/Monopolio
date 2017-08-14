847da722226185103459d93116f40b1d09d41eb6
Edwin F. Boza <eboza@espol.edu.ec>
2017-08-14 11:33:48 -0500

    Test3 para cuando es CHIRO y no le alcanza la plata.

--- test.py
+++ test.py
@@ -10,13 +10,21 @@ class Test(unittest.TestCase):
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
 		
-    #Test21 valida cuando es No es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    #Test2 valida cuando es No es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
     def test2(self):
         celda3 = Celda("", 1, "Carrito", 150, 50)
         persona3 = Persona("Toreto", 160,"si")
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"No Valido")
+		
+    #Test3 valida cuando es una propiedad, NO tiene dinero suficiente, decide comprar la propiedad entonces continua en el juego.
+    def test3(self):
+        celda3 = Celda("", 1, "Propiedad", 150, 50)
+        persona3 = Persona("Toreto", 120,"si")
+        juego3 = Juego()
+        msg = juego3.validar_movimiento(celda3,persona3)
+        self.assertEquals(msg,"No tiene dinero suficiente")
 		
 if __name__ == '__main__':
     unittest.main()
