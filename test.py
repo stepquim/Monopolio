c2397ca1752ff23360eaf4f68025db1fdb51711f
chrisvergaram <chrisvergaram@gmail.com>
2017-08-14 17:12:29 -0500

    Test2

--- test.py
+++ test.py
@@ -12,5 +12,17 @@ class Test(unittest.TestCase):
         juego3 = Juego()
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
+		
+		
+class Test2(unittest.TestCase):
+    #Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    def test2(self):
+        celda2 = Celda("", 2, "Carcel", 100, 60)
+        persona2 = Persona("Christian", 160,"si")
+        juego2= Juego()
+        msg = juego2.validar_movimiento(celda2,persona2)
+        self.assertEquals(msg,"No valido")
+
+		
 if __name__ == '__main__':
     unittest.main()
