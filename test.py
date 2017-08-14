e0e4e21de8e6654099ef6a2e882cad1a8fe00064
chrisvergaram <chrisvergaram@gmail.com>
2017-08-14 17:26:03 -0500

    Pruebas de caja blanca

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class Test(unittest.TestCase):
 		
 		
 class Test2(unittest.TestCase):
-    #Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    #Test2 valida que no es una propiedad
     def test2(self):
         celda2 = Celda("", 2, "Carcel", 100, 60)
         persona2 = Persona("Christian", 160,"si")
@@ -23,6 +23,23 @@ class Test2(unittest.TestCase):
         msg = juego2.validar_movimiento(celda2,persona2)
         self.assertEquals(msg,"No Valido")
 
+class Test4(unittest.TestCase):
+    #Test1 valida cuando es una propiedad, no tiene dinero
+    def test2(self):
+        celda4 = Celda("", 3, "Propiedad", 150, 60)
+        persona4 = Persona("Christian", 100,"si")
+        juego4= Juego()
+        msg = juego4.validar_movimiento(celda4,persona4)
+        self.assertEquals(msg,"No tiene dinero suficiente")
+
+class Test5(unittest.TestCase):
+    #Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    def test1(self):
+        celda3 = Celda("", 1, "Propiedad", 150, 50)
+        persona3 = Persona("Toreto", 160,"no")
+        juego3 = Juego()
+        msg = juego3.validar_movimiento(celda3,persona3)
+        self.assertEquals(msg,"Sigue jugando")
 		
 if __name__ == '__main__':
     unittest.main()
