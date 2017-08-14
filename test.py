f8d3f727e93c1bf18490f140c0b934b1d4577d25
Edwin F. Boza <eboza@espol.edu.ec>
2017-08-14 11:24:37 -0500

    Test2 para cuando NO es propiedad.

--- test.py
+++ test.py
@@ -10,5 +10,13 @@ class Test(unittest.TestCase):
         msg = juego3.validar_movimiento(celda3,persona3)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
 		
+    #Test21 valida cuando es No es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
+    def test2(self):
+        celda3 = Celda("", 1, "Carrito", 150, 50)
+        persona3 = Persona("Toreto", 160,"si")
+        juego3 = Juego()
+        msg = juego3.validar_movimiento(celda3,persona3)
+        self.assertEquals(msg,"la celda no es una Propiedad")
+		
 if __name__ == '__main__':
     unittest.main()
