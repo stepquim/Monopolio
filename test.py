133ad4824e7a2107a192c90b7690604b488b0f2a
Stephany <step-quim@hotmail.com>
2017-08-16 12:04:28 -0500

    update test1

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ from Monopolio import *
 class Test(unittest.TestCase):
     #Test1 valida cuando es una propiedad, tiene dinero, decide comprar la propiedad entonces continua en el juego.
     def test1(self):
-        celda3 = Celda("", 1, "Propiedad", 150, 50)
-        persona3 = Persona("Toreto", 160,"si")
-        juego3 = Juego()
-        msg = juego3.validar_movimiento(celda3,persona3)
+        celda1 = Celda("", 1, "Propiedad", 150, 50)
+        persona1 = Persona("Toreto", 160,"si")
+        juego = Juego()
+        msg = juego.validar_movimiento(celda1,persona1)
         self.assertEquals(msg,"Usted ha comprado una propiedad")
 		
 if __name__ == '__main__':
